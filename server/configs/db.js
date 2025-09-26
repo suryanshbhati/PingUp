f9a61baa3a55265dc51645e2454b9a637b798ba4
Suryansh Bhati <suryanshbhati20@gmail.com>
2025-09-27 00:51:00 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async() => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('Database connected');
+        });
+        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectDB;
