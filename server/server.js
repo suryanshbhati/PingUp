aa499dd9c9387b6c41a0630e67acf071a2239348
Suryansh Bhati <suryanshbhati20@gmail.com>
2025-10-03 13:28:01 +0530

    email reminder added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { serve } from "inngest/express";
 import {inngest, functions} from './inngest/index.js';
+import { clerkMiddleware } from '@clerk/express';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -11,12 +13,14 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get("/", (req,res) => {
     res.send('Server is running');
 });
 
 app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/user', userRouter);
 
 const PORT = process.env.PORT || 4000;
 
