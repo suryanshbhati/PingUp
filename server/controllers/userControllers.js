780b30bef1ff2fe9f627ccffa3e9227e3937eff0
Suryansh Bhati <suryanshbhati20@gmail.com>
2025-10-04 04:18:34 +0530

    backend complete

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,7 @@
 import imagekit from "../configs/imageKit.js";
+import { inngest } from "../inngest/index.js";
 import Connection from "../models/Connection.js";
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 import fs from 'fs';
 
@@ -185,10 +187,16 @@ export const sendConnectionRequest = async (req,res) => {
         });
 
         if(!connection){
-            await Connection.create({
+            const newConnection = await Connection.create({
                 from_user_id: userId,
                 to_user_id: id,
             });
+
+            await inngest.send({
+                name: 'app/connection-request',
+                data: {connectionId : newConnection._id}
+            });
+
             return res.json({success: true, message: 'Connection request sent successfully'});
         }else if(connection && connection.status == 'accepted'){
             return res.json({success: false, message: 'You are already connected with this user'});
@@ -243,6 +251,23 @@ export const acceptConnectionRequest = async (req,res) => {
         await connection.save();
 
         res.json({success: true, message: 'Connection accepted successfully'});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Get User Profiles
+export const getUserProfiles = async (req,res) => {
+    try {
+        const { profileId } = req.body;
+        const profile = await User.findById(profileId);
+        if(!profile){
+            return res.json({success: false, message: "Profile not found"});
+        }
+        const posts = await Post.find({user: profileId}).populate('user');
+
+        res.json({success: true, profile, posts});
     } catch (error) {
         console.log(error);
         res.json({success: false, message: error.message});
